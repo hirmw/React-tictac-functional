450f512a1e2eb56e6d27f704bcbde176b4a01430
hirmw <rich.woodhouse@outlook.com>
2020-04-13 16:42:46 +0100

    logic sorted

--- src/App.js
+++ src/App.js
@@ -1,68 +1,121 @@
-import React, { useState } from 'react';
-import Option from './Components/Option';
+import React, { useState, useReducer } from 'react';
 import Header from './Components/Header';
 
-const Tictak = () => {
-
- const [opponents, setopponents] = useState([
-   {choice: 'Rock', weight:0 },
-   {choice: 'Rock', weight:1 },
-   {choice: 'Rock', weight:2 },
-   
- ],
- {result:''},
- {comp_choice: ''},
- {logic: [
-  //rock
-  { Rock: ['Draw', 'Lost', 'Win'] },
-  //paper
-  { Paper: ['Win', 'Drawn', 'Loss'] },
-  //scissors
-  { Scissors: ['Loss', 'Won', 'Draw'] },
-]},
-{visible: 'hidden'},
-);
-
-//do i need const
- const compChoice = (cc) => {
-  switch(cc) {
-      case 0:
-        return 'Rock'
-      case 1:
-          return 'Paper'
-      case 2:
-          return 'Scissors'
-    }
-}
-
-const divStyleTitle = () => {
-  setopponents({visible: 'visible'})
-}
-
-const handleClick = (a, b) =>  {    
-  const computer_choice = Math.floor(Math.random() * 3) + 0;
-  const rest = compChoice(computer_choice)
-
-  setopponents({comp_choice: rest })
-  //console.log('comp cho' + rest)
-  console.log(computer_choice)
-  const x = opponents[a][b][computer_choice]
-  setopponents({result: x})
-  divStyleTitle()
+const divStyle = {
+  background: '#f4f4f4',
+  padding: '10px',
+  borderBottom: '1px #ccc dotted',
+  textAlign: 'center',
 
 };
 
-  return(
-    <div>
-        <Header> </Header>
-        <Option
-          handleClick={handleClick}
-          opponents={opponents}
-        />
-        <div> The Computer chose {opponents.comp_choice} , therefore you have {opponents.result}</div>
-    </div>
+const btnStyle = {
+background: '#ff0000',
+color: '#fff',
+border: '10px',
+padding: '10px 20px',
+borderRadius: '10%',
+cursor: 'pointer',
+float: 'center',
+};
 
-  )
+
+function AddOption({ op, divStyle, btnStyle, handleClick }) {
+
+  const { choice, weight } = op;
+
+  return (
+    <div style={divStyle}>
+      <button style={btnStyle} onClick={() => handleClick(choice, weight)}>
+        {' '}
+        {choice}
+        {' '}
+      </button>
+    </div>
+  );
+}
+
+function Tictak() {
+  const [opponents, setopponents] = useState([
+    { choice: 'Rock', weight: 0 },
+    { choice: 'Paper', weight: 1 },
+    { choice: 'Scissors', weight: 2 }
+  ]);
+
+  const [state, setstate] = useState({
+    result: '',
+    visible: 'hidden',
+  });
+
+  const [compstate, setcompstate] = useState({
+    comp_choice: '',
+  });
+
+  // const [logic, setlogic] = useState(options);
+  function names(names, action) {
+    switch (action.type) {
+      case "getscroes":
+        console.log(action.weight)
+        console.log(action.choice)
+        return [names];
+    }
+  }
+
+  let initialstate = { 
+    result: [{Rock: ['Draw', 'Win', 'Lost'], Paper: ['Win', 'Draw', 'Win'], Scissors: ['Loss', 'Win', 'Draw']}]
+  };
+
+  const [count, dispatch] = useReducer(names, initialstate);
+
+
+  //do i need const
+  const compChoice = cc => {
+    switch (cc) {
+      case 0:
+        return 'Rock';
+      case 1:
+        return 'Paper';
+      case 2:
+        return 'Scissors';
+    }
+  };
+
+  const divStyleTitle = () => {
+    setopponents({ visible: 'visible' });
+  };
+  
+  const handleclick = (a, b) => {
+    console.log(a,b + 'clicked')
+    const computer_choice = Math.floor(Math.random() * 3) + 0;
+    const computer_choiceString = compChoice(computer_choice)
+    console.log('computer_choice ' + computer_choice)
+    console.log('computer_choice ' + computer_choiceString)
+    setcompstate({comp_choice: computer_choiceString})
+    const get_result = count.result[0][a][computer_choice]
+    console.log(count.result[0][a]);
+    setstate({result: get_result})
+  }
+
+
+  return (
+
+    <div style={divStyle}>
+      <Header> </Header>
+      {opponents.map((op) => (
+        <AddOption handleClick={handleclick} op={op} btnStyle={btnStyle} />
+      ))}
+
+      <div>
+    
+      </div>
+     
+      <div>
+        {' '}
+        The Computer chose {compstate.comp_choice} , therefore you have {state.result}
+        
+      </div>
+    </div>
+  );
 }
 
 export default Tictak;
--- src/App.old.js
+++ src/App.old.js
@@ -1,80 +1,97 @@
-import React, { Component } from 'react';
-import Option from './Components/Option';
+import React, { useState } from 'react';
 import Header from './Components/Header';
 
-export class tictak extends Component {
-  constructor(props) {
-    super(props);
+const Tictak = () => {
 
-    this.state = {
-      opponents: [
-        { choice: 'Rock', weight: 0 },
-        { choice: 'Paper', weight: 1 },
-        { choice: 'Scissors', weight: 2 },
-      ],
-      result: '',
-      comp_choice: '',
-      logic: [
-        //rock
-        { Rock: ['Draw', 'Lost', 'Win'] },
-        //paper
-        { Paper: ['Win', 'Drawn', 'Loss'] },
-        //scissors
-        { Scissors: ['Loss', 'Won', 'Draw'] },
-      ],
-      visible: 'hidden',
-    };
-  }
+ const [opponents, setopponents] = useState([
+   {choice: 'Rock', weight:0 },
+   {choice: 'Paper', weight:1 },
+   {choice: 'Scissors', weight:2 },   
+ ])
 
-    compChoice = (cc) => {
-        switch(cc) {
-            case 0:
-              return 'Rock';
-            case 1:
-                return 'Paper'
-            case 2:
-                return 'Scissors'
-          }
+ const [state, setstate] = useState(   
+    {result:'', comp_choice: '', visible: 'hidden'},    
+ )
+
+ 
+ const [logic, setlogic] = useState(
+  [
+    //rock
+    { Rock: ['Draw', 'Lost', 'Win'] },
+    //paper
+    { Paper: ['Win', 'Drawn', 'Loss'] },
+    //scissors
+    { Scissors: ['Loss', 'Win', 'Draw'] },
+  ]
+ )
+
+
+
+//do i need const
+ const compChoice = (cc) => {
+  switch(cc) {
+      case 0:
+        return 'Rock'
+      case 1:
+          return 'Paper'
+      case 2:
+          return 'Scissors'
     }
-
-    handleClick = (a, b) =>  {    
-        const computer_choice = Math.floor(Math.random() * 3) + 0;
-        const rest = this.compChoice(computer_choice)
-
-        this.setState({comp_choice: rest })
-        //console.log('comp cho' + rest)
-        console.log(computer_choice)
-        const x = this.state.logic[a][b][computer_choice]
-        this.setState({result: x})
-        this.divStyleTitle()
-
-  };
-
-    divStyleTitle = () => {
-            this.setState({visible: 'visible'})
-    }
-
-  render() {
-    const divStyleTitle = {
-        background: '#f4f4f4',
-        padding: '10px',
-        borderBottom: '1px #ccc dotted',
-        textAlign: 'center',
-        visibility: this.state.visible
-    };
-    return (
-      <div >
-        <Header> </Header>
-        <Option
-          handleClick={this.handleClick}
-          opponents={this.state.opponents}
-        />
-        <div style={divStyleTitle}> The Computer chose {this.state.comp_choice} , therefore you have {this.state.result}</div>
-        </div>
-
-    );
-
-  }
 }
 
-export default tictak;
+const divStyleTitle = () => {
+  setopponents({visible: 'visible'})
+}
+
+const handleClick = (a, b) =>  {   
+        console.log(a,b) 
+        const computer_choice = Math.floor(Math.random() * 3) + 0;
+        //const rest = compChoice(computer_choice)
+
+       //setstate({comp_choice: computer_choice });
+        //console.log('comp cho' + rest)
+        console.log(computer_choice)
+        const x = logic[a][b][computer_choice]
+        setstate({result: x});
+        divStyleTitle()
+};
+
+// const Option = (opponents, result, handleClick) => {
+//   return opponents.map(op => (
+//     <AddOption
+//       op={op}
+//       result={result}
+//       handleClick={handleClick}
+//     />
+//   ));
+// }
+
+
+const AddOption = (op, divStyle, btnStyle, handleClick) => {
+
+  const { choice, weight } = op;
+
+
+  return (
+    <div style={divStyle}>  
+        <button style={btnStyle} onClick={handleClick(weight, choice)}>  {choice} </button>
+    </div>
+  );
+  
+}
+
+  return(
+    <div>
+        <Header> </Header>
+        <Option
+          handleClick={handleClick}
+          opponents={opponents}
+          result={state.result}
+        />
+        <div> The Computer chose {state.comp_choice} , therefore you have {state.result}</div>
+    </div>
+
+  )
+}
+
+export default Tictak;
